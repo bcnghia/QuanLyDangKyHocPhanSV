60e02263ed80aef44037aa9e54dfedd5c296081c
khangksp <129304975+khangksp@users.noreply.github.com>
2024-07-21 10:20:25 +0700

    thembulon

--- QuanLyDangKyHocPhanSV/QuanLyDangKyHocPhanSV/obj/Debug/net8.0/QuanLyDangKyHocPhanSV.AssemblyInfo.cs
+++ QuanLyDangKyHocPhanSV/QuanLyDangKyHocPhanSV/obj/Debug/net8.0/QuanLyDangKyHocPhanSV.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QuanLyDangKyHocPhanSV")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c258eccf0dec0cda94ad4ef3dd636e3da208ef0b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+466ca1a7b83ae898d357b8aa2f3eddbf7a933d83")]
 [assembly: System.Reflection.AssemblyProductAttribute("QuanLyDangKyHocPhanSV")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QuanLyDangKyHocPhanSV")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
