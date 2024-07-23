7fc603b73203153a65c7d87d4154d5511443aa50
khangksp <129304975+khangksp@users.noreply.github.com>
2024-07-23 20:55:25 +0700

    Thêm đủ thứ ?

---
+++ QuanLyDangKyHocPhanSV/QuanLyDangKyHocPhanSV/Controllers/MonHocController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace QuanLyDangKyHocPhanSV.Controllers
+{
+    public class MonHocController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult DanhSachSinhVienDangKy()
+        {
+            return View();
+        }
+    }
+}
---
+++ QuanLyDangKyHocPhanSV/QuanLyDangKyHocPhanSV/Controllers/QLTaiKhoanController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace QuanLyDangKyHocPhanSV.Controllers
+{
+    public class QLTaiKhoanController : Controller
+    {
+        public IActionResult SinhVien()
+        {
+            return View();
+        }
+
+        public IActionResult GiangVien()
+        {
+            return View();
+        }
+
+        public IActionResult GiaoVu()
+        {
+            return View();
+        }
+    }
+}
