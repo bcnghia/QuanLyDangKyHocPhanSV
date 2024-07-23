2c4393ce405898bd28c6d7235f77785069b634c0
khangksp <129304975+khangksp@users.noreply.github.com>
2024-07-23 20:05:55 +0700

    fix

---
+++ QuanLyDangKyHocPhanSV/QuanLyDangKyHocPhanSV/Controllers/DangNhapController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace QuanLyDangKyHocPhanSV.Controllers
+{
+    public class DangNhapController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // Phương thức POST cho form đăng nhập
+        [HttpPost]
+        public IActionResult TaiKhoan(string email, string password)
+        {
+            // Thực hiện các thao tác đăng nhập ở đây
+            // Ví dụ: Kiểm tra thông tin đăng nhập, xác thực người dùng, v.v.
+
+            // Sau khi đăng nhập thành công, điều hướng về trang chủ
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
