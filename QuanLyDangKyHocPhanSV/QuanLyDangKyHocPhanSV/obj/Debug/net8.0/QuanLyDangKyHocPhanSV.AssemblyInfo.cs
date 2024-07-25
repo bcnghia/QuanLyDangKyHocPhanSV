aa0f9b56c5ffec8fdae273e27f1b3e4a8979d41b
khangksp <129304975+khangksp@users.noreply.github.com>
2024-07-25 19:55:55 +0700

    UI Trang chủ Part1

--- QuanLyDangKyHocPhanSV/QuanLyDangKyHocPhanSV/obj/Debug/net8.0/QuanLyDangKyHocPhanSV.AssemblyInfo.cs
+++ QuanLyDangKyHocPhanSV/QuanLyDangKyHocPhanSV/obj/Debug/net8.0/QuanLyDangKyHocPhanSV.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QuanLyDangKyHocPhanSV")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad53df0fed0ed36d694f51dce3880dae3c9fa8b5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d6921d80e7ab8d8722b92363d80c886818f24d8")]
 [assembly: System.Reflection.AssemblyProductAttribute("QuanLyDangKyHocPhanSV")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QuanLyDangKyHocPhanSV")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
