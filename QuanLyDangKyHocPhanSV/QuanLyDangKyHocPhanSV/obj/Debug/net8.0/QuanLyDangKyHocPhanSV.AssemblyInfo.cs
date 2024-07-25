496f21e639043de1e651badc11f26549e649dbad
DucTai1204 <22050090@student.bdu.edu.vn>
2024-07-25 21:18:25 +0700

    update ThemMonHoc ( thêm phần tkb, bắt lỗi âm )

--- QuanLyDangKyHocPhanSV/QuanLyDangKyHocPhanSV/obj/Debug/net8.0/QuanLyDangKyHocPhanSV.AssemblyInfo.cs
+++ QuanLyDangKyHocPhanSV/QuanLyDangKyHocPhanSV/obj/Debug/net8.0/QuanLyDangKyHocPhanSV.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QuanLyDangKyHocPhanSV")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d6921d80e7ab8d8722b92363d80c886818f24d8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa0f9b56c5ffec8fdae273e27f1b3e4a8979d41b")]
 [assembly: System.Reflection.AssemblyProductAttribute("QuanLyDangKyHocPhanSV")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QuanLyDangKyHocPhanSV")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
