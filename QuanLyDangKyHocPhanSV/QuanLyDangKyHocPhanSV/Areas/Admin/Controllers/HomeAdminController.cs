d836ed2041e540c9ecb9dd805a2d2fb10a5ade2a
Bui Cong Nghia <bcongnghia2310@gmail.com>
2024-07-26 22:11:39 +0700

    fix xong đống lỗi cho admin, push cho chắc

---
+++ QuanLyDangKyHocPhanSV/QuanLyDangKyHocPhanSV/Areas/Admin/Controllers/HomeAdminController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using QuanLyDangKyHocPhanSV.Data;
+
+namespace QuanLyDangKyHocPhanSV.Areas.Admin.Controllers
+{
+    [Area("admin")]
+    [Route("admin")]
+    [Route("admin/homeadmin")]
+    public class HomeAdminController : Controller
+    {
+        QLDKHocPhanContext db = new QLDKHocPhanContext();
+        [Route("")]
+        [Route("index")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("danhsachmonhoc")]
+        public IActionResult DanhSachMonHoc()
+        {
+            var lstMonHoc = db.MonHocs.ToList();
+            return View(lstMonHoc);
+        }
+    }
+}
--- QuanLyDangKyHocPhanSV/QuanLyDangKyHocPhanSV/Models/MonHoc.cs
+++ QuanLyDangKyHocPhanSV/QuanLyDangKyHocPhanSV/Models/MonHoc.cs
@@ -1,18 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace QuanLyDangKyHocPhanSV.Models;
 
 public partial class MonHoc
 {
+    [Display(Name = "Mã Môn Học")]
     public string MaMh { get; set; } = null!;
 
+    [Display(Name = "Tên Môn Học")]
     public string TenMh { get; set; } = null!;
 
+    [Display(Name = "Số Tiết")]
     public int SoTiet { get; set; }
 
+    [Display(Name = "Số Tín Chỉ")]
     public int SoTinChi { get; set; }
 
+    [Display(Name = "Thời Khóa Biểu")]
     public string? ThoiKhoaBieu { get; set; }
 
     public virtual ICollection<DangKyMonHoc> DangKyMonHocs { get; set; } = new List<DangKyMonHoc>();
