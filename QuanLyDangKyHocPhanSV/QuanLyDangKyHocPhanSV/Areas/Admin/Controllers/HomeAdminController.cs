d603bcf5bd575e4ffd73c5f2eb710733d7fbc9d0
Bui Cong Nghia <bcongnghia2310@gmail.com>
2024-07-27 00:57:13 +0700

    Big Update Admin UI
    localhost:port/admin để gọi trang admin

---
+++ QuanLyDangKyHocPhanSV/QuanLyDangKyHocPhanSV/Areas/Admin/Controllers/HomeAdminController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using QuanLyDangKyHocPhanSV.Data;
+
+namespace QuanLyDangKyHocPhanSV.Areas.Admin.Controllers
+{
+    [Area("admin")]
+    [Route("admin")]
+    [Route("admin/homeadmin")]
+    public class HomeAdminController : Controller
+    {
+        QLDKHocPhanContext db = new QLDKHocPhanContext();
+        [Route("")]
+        [Route("index")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("danhsachmonhoc")]
+        public IActionResult DanhSachMonHoc()
+        {
+            var lstMonHoc = db.MonHocs.ToList();
+            return View(lstMonHoc);
+        }
+
+        [Route("giangvien")]
+        public IActionResult GiangVien()
+        {
+            var lstGiangVien = db.GiangViens.ToList();
+            return View(lstGiangVien);
+        }
+
+        [Route("sinhvien")]
+        public IActionResult SinhVien()
+        {
+            var lstSinhVien = db.SinhViens.ToList();
+            return View(lstSinhVien);
+        }
+
+        [Route("giaovu")]
+        public IActionResult GiaoVu()
+        {
+            var lstGiaoVu = db.GiaoVus.ToList();
+            return View(lstGiaoVu);
+        }
+
+        [Route("sinhviendangkymonhoc")]
+        public IActionResult SinhVienDangKyMonHoc()
+        {
+            var lstSinhVienDangKyMonHoc = db.VThongTinDangKyMonHocs.ToList();
+            return View(lstSinhVienDangKyMonHoc);
+        }
+    }
+}
