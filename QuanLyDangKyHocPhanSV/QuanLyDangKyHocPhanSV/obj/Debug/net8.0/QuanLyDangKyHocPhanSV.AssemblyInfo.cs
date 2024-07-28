e1002923b6d5188b803e6e29a1d01c3a7441c166
khangksp <129304975+khangksp@users.noreply.github.com>
2024-07-28 13:27:54 +0700

    dunnu

--- QuanLyDangKyHocPhanSV/QuanLyDangKyHocPhanSV/obj/Debug/net8.0/QuanLyDangKyHocPhanSV.AssemblyInfo.cs
+++ QuanLyDangKyHocPhanSV/QuanLyDangKyHocPhanSV/obj/Debug/net8.0/QuanLyDangKyHocPhanSV.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QuanLyDangKyHocPhanSV")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e819933987c9226de0c6daa1d05a3ca96380935")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc2230a93f6835af531b20accbe63dea2c97dd7f")]
 [assembly: System.Reflection.AssemblyProductAttribute("QuanLyDangKyHocPhanSV")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QuanLyDangKyHocPhanSV")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- QuanLyDangKyHocPhanSV/QuanLyDangKyHocPhanSV/obj/Release/net8.0/QuanLyDangKyHocPhanSV.AssemblyInfo.cs
+++ QuanLyDangKyHocPhanSV/QuanLyDangKyHocPhanSV/obj/Release/net8.0/QuanLyDangKyHocPhanSV.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QuanLyDangKyHocPhanSV")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b5d74cecb0c4881701d6ea554017836e331286c1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc2230a93f6835af531b20accbe63dea2c97dd7f")]
 [assembly: System.Reflection.AssemblyProductAttribute("QuanLyDangKyHocPhanSV")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QuanLyDangKyHocPhanSV")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
