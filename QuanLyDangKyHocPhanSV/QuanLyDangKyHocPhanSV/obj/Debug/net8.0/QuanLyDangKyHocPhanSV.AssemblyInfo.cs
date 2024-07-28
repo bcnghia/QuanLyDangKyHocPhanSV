ebbac56939de8972ea019c6abef6d6041804fd22
khangksp <129304975+khangksp@users.noreply.github.com>
2024-07-28 17:31:34 +0700

    Chỉnh giao diện Admin (1 tý)

--- QuanLyDangKyHocPhanSV/QuanLyDangKyHocPhanSV/obj/Debug/net8.0/QuanLyDangKyHocPhanSV.AssemblyInfo.cs
+++ QuanLyDangKyHocPhanSV/QuanLyDangKyHocPhanSV/obj/Debug/net8.0/QuanLyDangKyHocPhanSV.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QuanLyDangKyHocPhanSV")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc2230a93f6835af531b20accbe63dea2c97dd7f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e1002923b6d5188b803e6e29a1d01c3a7441c166")]
 [assembly: System.Reflection.AssemblyProductAttribute("QuanLyDangKyHocPhanSV")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QuanLyDangKyHocPhanSV")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
